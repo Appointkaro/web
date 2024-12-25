d59d672135c4a3d2f765a004f904f6c92146a755
Reshma-dev-hash <reshma.kakimanu@sithafal.com>
2024-12-25 22:23:22 +0530

    commit

--- src/components/TwoFactorAuthentication.js
+++ src/components/TwoFactorAuthentication.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './TwoFactorAuthentication.css';
 import { useNavigate } from "react-router-dom";
+import Logo from '../assets/images/Appoint.png'
 
 function TwoFactorAuthentication() {
   const [otp, setOtp] = useState("");
@@ -20,7 +21,7 @@ function TwoFactorAuthentication() {
   return (
     <div className="container">
       <div className="frame">
-        <img src="src/your-image.jpg" alt="2FA Image" className="image" />
+        <img src={Logo} alt="2FA Image" className="image" />
         <div className='text'>
         <p className="text">Enter the  OTP sent to ******9090</p>
       </div>
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./signUp.css"; // Import the CSS for styling
 import { Link } from "react-router-dom";
 import Logo from '../assets/images/Appoint.png'
+import { useNavigate } from "react-router-dom";
 
 
 const SignUp = () => {
@@ -12,24 +13,53 @@ const SignUp = () => {
     password: "",
     confirmPassword: "",
   });
-
+  const [errors, setErrors] = useState({
+    name: '',
+    mobile: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+ const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   };
-
+  const validate = () => {
+    const newErrors = {};
+    const flag = false
+    if(!formData.name){
+      newErrors.name = 'Name is required';
+      flag = true
+    } 
+    if(!formData.mobile){
+      newErrors.mobile = 'Mobile Number is required';
+      flag = true
+    } 
+    if(!formData.email){
+      newErrors.email = 'Email is required';
+      flag = true
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = 'Email is invalid';
+      flag = true
+    }
+    if (formData.password !== formData.confirmPassword) {
+      newErrors.confirmPassword = 'Passwords do not match!';
+      flag = true
+    } 
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (formData.password !== formData.confirmPassword) {
-      alert("Passwords do not match!");
-    } else {
-      alert("Account Created Successfully!");
-      // Handle account creation logic here
+    if (validate()) {
+      console.log('Form data submitted:', formData);
+      navigate('/');
     }
   };
-
+ 
   return (
     <><div className="container">
           <img src={Logo} alt="Centered Image" className="top-middle-image" />
@@ -44,6 +74,8 @@ const SignUp = () => {
                       value={formData.name}
                       onChange={handleChange}
                       required />
+                  {errors.name && <span className="error">{errors.name}</span>}
+
                   <input
                       type="tel"
                       name="mobile"
@@ -52,13 +84,16 @@ const SignUp = () => {
                       onChange={handleChange}
                       pattern="[0-9]{10}"
                       required />
+                  {errors.mobile && <span className="error">{errors.mobile}</span>}
+
                   <input
                       type="email"
                       name="email"
-                      placeholder="Email"
+                      placeholder="Enter your email"
                       value={formData.email}
                       onChange={handleChange}
                       required />
+                  {errors.email && <span className="error">{errors.email}</span>}
                   <input
                       type="password"
                       name="password"
@@ -66,6 +101,8 @@ const SignUp = () => {
                       value={formData.password}
                       onChange={handleChange}
                       required />
+                  {errors.confirmPassword && <span className="error">{errors.confirmPassword}</span>}
+
                   <input
                       type="password"
                       name="confirmPassword"
@@ -73,7 +110,7 @@ const SignUp = () => {
                       value={formData.confirmPassword}
                       onChange={handleChange}
                       required />
-                  <button type="submit" className="create-account-btn">
+                  <button type="submit" className="create-account-btn" >
                       CREATE ACCOUNT
                   </button>
               </form>
